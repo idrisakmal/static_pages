b668dd7a352ac7c59fe6b5794235068a599588e7
idrisakmal <simple_idris@yahoo.com>
2015-05-19 16:48:00 +0800

    add a static pages controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,14 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
   test "should get home" do
     get :home
-    assert_select "title", "Home | #{@base_title}"
+    assert_response :success
   end
 
   test "should get help" do
     get :help
-    assert_select "title", "Help | #{@base_title}"
-  end
-
-  test "should get about" do
-    get :about
-    assert_select "title", "About | #{@base_title}"
+    assert_response :success
   end
 
 end
