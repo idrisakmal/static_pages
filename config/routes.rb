a49403de300899252937c43ca064209f3af0ecf5
idrisakmal <simple_idris@yahoo.com>
2015-05-20 11:42:07 +0800

    Final Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/home'
+  root 'static_pages/home'
 
   get 'static_pages/help'
 
@@ -14,10 +10,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
-  get 'home' => 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
